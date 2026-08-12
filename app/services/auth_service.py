f0413acc88f26f8e5837f7a63f258001a1418b10
etechpro <technov8ive@gmail.com>
2026-08-12 20:20:56 +0100

    feat: add auth service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,3 +1,4 @@
 """auth service is a utility function to hash a password coming from the user.
 And another utility to verify if a received password matches the hash stored.
+And another one to authenticate and return a user.
 """
