0ddb98d18bb171178cc681af363a96dc55464501
etechpro <technov8ive@gmail.com>
2026-08-13 20:23:13 +0100

    feat: add auth service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -2,3 +2,4 @@
 And another utility to verify if a received password matches the hash stored.
 And another one to authenticate and return a user.
 """
+from datetime import datetime, timedelta, timezone
