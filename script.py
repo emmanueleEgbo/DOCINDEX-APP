fd2e3bff7e65fd51b8f425c02a3a2ae24e069933
etechpro <technov8ive@gmail.com>
2026-08-14 22:41:46 +0100

    feat: add auth service

--- script.py
+++ script.py
@@ -1,1 +1,4 @@
 """A script for generating auth secret key"""
+
+ # run python -c "import secrets; print(secrets.token_hex(32))"
+# to get something like this 2f257e3dc2d2fde16cd17fe1f7e9d695169f32fbb10d7b91c322ffa92df036ay
