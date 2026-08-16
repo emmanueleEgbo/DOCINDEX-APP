c5d3e652263ba2b4aa873d82a3f77a28dfcd313f
etechpro <technov8ive@gmail.com>
2026-08-16 01:58:35 +0100

    feat: add auth service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -35,5 +35,10 @@ DUMMY_HASH = password_hash.hash("dummypassword")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+
 def verify_password(plain_password, hashed_password):
     return password_hash.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return password_hash.hash(password)
