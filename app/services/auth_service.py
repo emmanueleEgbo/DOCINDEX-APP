f80f074cf4d4d9d09f85c61d50f6689dd40da268
etechpro <technov8ive@gmail.com>
2026-08-19 23:04:51 +0100

    feat: add auth service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -47,3 +47,7 @@ def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+   
