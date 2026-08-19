7acd00b5620493aef04cc7206650b15ba5825ef0
etechpro <technov8ive@gmail.com>
2026-08-19 23:06:33 +0100

    feat: add auth service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -53,4 +53,6 @@ def authenticate_user(fake_db, username: str, password: str):
     if not user:
            verify_password(password, DUMMY_HASH)
            return False
-     
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
