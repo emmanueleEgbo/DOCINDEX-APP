00321e396f3a30dcbb172fa2e3b6be84172c4f23
etechpro <technov8ive@gmail.com>
2026-08-20 23:13:03 +0100

    feat: add auth service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -63,3 +63,6 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
   
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+   
