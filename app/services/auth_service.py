53767a97bd2ab0e3bdbc6f19a43078e8da08ebb0
etechpro <technov8ive@gmail.com>
2026-08-21 23:20:04 +0100

    feat: add auth service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -70,4 +70,11 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt    
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    pass
+        credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+        )
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
