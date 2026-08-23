83dda486085967e487f4e08089ee29a79ffe2227
etechpro <technov8ive@gmail.com>
2026-08-23 23:25:12 +0100

    feat: add auth service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -81,5 +81,8 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
             if username is None:
                 raise credentials_exception
             token_data = TokenData(username=username)
+        except InvalidTokenError:
+            raise credentials_exception
+        user = get_user(fake_users_db, username=token_data.username)
 
 
