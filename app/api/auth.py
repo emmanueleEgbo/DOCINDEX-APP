2458b700f6c7979adcda64f2de64c31e526a29d2
etechpro <technov8ive@gmail.com>
2026-09-01 03:31:34 +0100

    feat: add auth service

--- app/api/auth.py
+++ app/api/auth.py
@@ -6,6 +6,7 @@ async def login_for_access_token(
 ) -> Token:
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
     if not user:
-          
-    )
+        raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    
     
