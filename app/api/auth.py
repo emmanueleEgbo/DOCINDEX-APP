ac4e79c904816e9a6c6cf018a06faeeffda15c60
etechpro <technov8ive@gmail.com>
2026-09-01 03:32:10 +0100

    feat: add auth service

--- app/api/auth.py
+++ app/api/auth.py
@@ -7,6 +7,8 @@ async def login_for_access_token(
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                    
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+    )
     
