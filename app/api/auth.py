f7d8a77c5f5954e4f1578626f831f83210235cab
etechpro <technov8ive@gmail.com>
2026-09-04 00:57:09 +0100

    feat: add auth service

--- app/api/auth.py
+++ app/api/auth.py
@@ -19,5 +19,5 @@ async def login_for_access_token(
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
-        
+    return Token(access_token=access_token, token_type="bearer") 
    
