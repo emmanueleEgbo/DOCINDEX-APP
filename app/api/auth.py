2cde3f2df9b9e14e53014928322117c9e2f0a60a
etechpro <technov8ive@gmail.com>
2026-09-03 03:35:18 +0100

    feat: add auth service

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,7 +1,7 @@
 """A auth route for ...."""
 from typing import Annotated
 from app.schemas.user_auth_schema import Token
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
