c7a889afe9598b333ef55ca3f10aa1194a7e02ec
etechpro <technov8ive@gmail.com>
2026-09-05 12:26:16 +0100

    feat: add auth service

--- app/api/auth.py
+++ app/api/auth.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from typing import Annotated
 from fastapi import Depends, HTTPException, status, APIRouter
 from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.schemas.user_auth_schema import Token
 from app.services.auth_service import authenticate_user, create_access_token
@@ -16,17 +16,21 @@ router = APIRouter(tags=['Authentication'])
 
 
 @router.post("/token", status_code=status.HTTP_201_CREATED)
-async def login_for_access_token(user: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)) -> Token:
-    user = authenticate_user(db, user.username, user.password)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Annotated[AsyncSession, Depends(get_db)],) -> Token:
+    user = await authenticate_user(db, form_data.username, form_data.password)
+
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    
     access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+
+    # CREATE JWT
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"sub": user.email}, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
    
